7e8fc5d3584d0ec0dc1b83dcff68aa8a8c8b4afc
UD0NGD0NG <156397027+UD0NGD0NG@users.noreply.github.com>
2024-01-16 10:56:24 +0900

    Comment processing

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -8,14 +8,14 @@ int main() {
 	queue<int> q;
 	int n, m, a, b, i, max = 0;
 	cin >> n >> m;
-	vector<vector<int>> edge(n + 1);
-	vector<int> cnt(n + 1, 0);
+	vector<vector<int>> edge(n + 1); // 간선을 저장하는 vector
+	vector<int> cnt(n + 1, 0); // 해킹한 컴퓨터의 개수를 저장하는 vector
 	while (m--) {
 		cin >> a >> b;
-		edge[b].push_back(a); // 그래프 방향 다시 생각해보기
+		edge[b].push_back(a); // b를 해킹시 a도 해킹되지만 a를 해킹한다해서 b가 해킹되지는 않음 -> 단방향 그래프
 	}
 	for (i = 1; i <= n; i++) {
-		vector<bool> visit(n + 1, false);
+		vector<bool> visit(n + 1, false); // 시작 node가 바뀔때마다 visit 초기화
 		q.push(i);
 		visit[i] = true;
 		while (!q.empty()) {
@@ -25,7 +25,7 @@ int main() {
 				if (!visit[y]) {
 					visit[y] = true;
 					q.push(y);
-					cnt[i]++;
+					cnt[i]++; // 시작 node의 cnt vector값을 바꿔줘야함
 				}
 			}
 		}
@@ -33,7 +33,7 @@ int main() {
 	for (i = 1; i <= n; i++)
 		if (max < cnt[i])
 			max = cnt[i];
-	for (i = 1; i <= n; i++)
+	for (i = 1; i <= n; i++) // 자동 오름차순
 		if (max == cnt[i])
 			cout << i << ' ';
 }
