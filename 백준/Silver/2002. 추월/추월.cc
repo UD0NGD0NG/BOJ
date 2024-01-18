85dfd0756544030d14e5b1b0196275851aa867fd
UD0NGD0NG <156397027+UD0NGD0NG@users.noreply.github.com>
2024-01-18 09:38:09 +0900

    Comment processing

--- 백준/Silver/2002. 추월/추월.cc
+++ 백준/Silver/2002. 추월/추월.cc
@@ -5,10 +5,10 @@ using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	map<string, int> mp;
-	vector<string> v;
+	map<string, int> mp; // 들어간 순서를 저장
+	vector<string> v; // 나온 순서를 저장
 	int n, i, j, cnt = 0;
-	string c;
+	string c; // 차량번호
 	cin >> n;
 	for (i = 0; i < n; i++) {
 		cin >> c;
@@ -20,8 +20,8 @@ int main() {
 	}
 	for (i = 0; i < n - 1; i++) {
 		for (j = i + 1; j < n; j++) {
-			if (mp[v[i]] > mp[v[j]]) {
-				cnt++;
+			if (mp[v[i]] > mp[v[j]]) { // 늦게 나온 차량 중 먼저 들어간 차량이 있다면
+				cnt++; // 추월한 차량 수 +1
 				break;
 			}
 		}
