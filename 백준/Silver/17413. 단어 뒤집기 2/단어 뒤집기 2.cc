586827795bc191ec382511b5d18d8754dc210221
UD0NGD0NG <156397027+UD0NGD0NG@users.noreply.github.com>
2024-01-19 14:12:41 +0900

    Add comments

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -6,8 +6,8 @@ using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	vector<char> v;
-	stack<char> st;
+	vector<char> v; // 결과를 저장
+	stack<char> st; // 부분 문자열을 뒤집기 위해 임시저장
 	int i;
 	bool flag = false;
 	string s;
@@ -21,36 +21,28 @@ int main() {
 			v.push_back(' ');
 			continue; // 공백을 line21에서 직접 넣었기에 밑에 코드 넘기기
 		}
-		if (s[i] == '<') {
+		if (s[i] == '<') { // <이 나올시 현재까지 입력된 문자 뒤집기
 			if (!st.empty()) {
 				while (!st.empty()) {
 					v.push_back(st.top());
 					st.pop();
 				}
 			}
-			flag = true; // 뒤에 오는 문자들은 뒤집지 않고 입력
+			flag = true; // <>안에 있음을 나타냄
 			v.push_back('<');
 			continue;  // <을 line32에서 직접 넣었기에 밑에 코드 넘기기
 		}
 		if (s[i] == '>') {
-			flag = false;
+			flag = false;  // <>을 벗어남을 나타냄
 			v.push_back('>');
 			continue;  // >을 line37에서 직접 넣었기에 밑에 코드 넘기기
 		}
-		if (flag) {
-			if (st.empty())
-				v.push_back(s[i]);
-			/*else {
-				while (!st.empty()) {
-					v.push_back(st.top());
-					st.pop();
-				}
-			}*/
-		}
+		if (flag)
+			v.push_back(s[i]); // 뒤집지 않고 저장
 		else
-			st.push(s[i]);
+			st.push(s[i]); // 뒤집기 위해 stack에 임시 저장
 	}
-	while (!st.empty()) {
+	while (!st.empty()) { // 마지막 문자가 >가 아닌경우를 위함
 		v.push_back(st.top());
 		st.pop();
 	}
