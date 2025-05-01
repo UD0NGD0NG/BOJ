d3f5964ca6f85986a3a86581708514be1156319c
UD0NGD0NG <156397027+UD0NGD0NG@users.noreply.github.com>
2025-05-01 16:27:45 +0900

    [Silver IV] Title: 싱크홀, Time: 0 ms, Memory: 2208 KB, Score: 150 point -BaekjoonHub

--- 백준/Silver/15830. 싱크홀/싱크홀.cc
+++ 백준/Silver/15830. 싱크홀/싱크홀.cc
@@ -1,20 +1,15 @@
 #include <iostream>
-#include <unordered_map>
+#include <cmath>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	double v, w, d; cin >> v >> w >> d;
+	long long v, w, d; cin >> v >> w >> d;
 
-	int cur = 0, ans = 0;
-	while (cur < d) {
-		cur += 5 * (w / v) * (w / v);
-
-		if (cur < d) {
-			ans++;
-			v *= 0.8;
-		}
+	long long ans = 0;
+	while (80 * w * w * pow(25, ans) - 80 * w * w * pow(16, ans) < 9 * d * v * v * pow(16, ans)) {
+		ans++;
 	}
 
-	cout << ans;
+	cout << ans - 1;
 }
