aeef1b93ac9714aea03bcef780a6bc5cd0708926
UD0NGD0NG <156397027+UD0NGD0NG@users.noreply.github.com>
2025-09-19 17:35:34 +0900

    [Silver I] Title: 1, 2, 3 더하기 7, Time: 32 ms, Memory: 9904 KB -BaekjoonHub

---
+++ 백준/Silver/15992. 1， 2， 3 더하기 7/1， 2， 3 더하기 7.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MOD = 1'000'000'009;
+
+int main() {
+	vector<vector<long long>> v(1'005, vector<long long>(1'005, 0));
+	v[1][1] = 1;
+	v[2][1] = 1; v[2][2] = 1;
+	v[3][1] = 1; v[3][2] = 2; v[3][3] = 1;
+
+	for (int i = 4; i <= 1'000; i++) {
+		for (int j = 1; j <= i; j++) {
+			v[i][j] = (v[i - 1][j - 1] + v[i - 2][j - 1] + v[i - 3][j - 1]) % MOD;
+		}
+	}
+
+	int t; cin >> t;
+	while (t--) {
+		int n, m; cin >> n >> m;
+		cout << v[n][m] << '\n';
+	}
+}
