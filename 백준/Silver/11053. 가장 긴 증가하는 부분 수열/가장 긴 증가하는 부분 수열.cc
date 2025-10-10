739d2f397fc928a1d180b0eb99972dec6546ffb5
UD0NGD0NG <156397027+UD0NGD0NG@users.noreply.github.com>
2025-10-10 12:40:35 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,25 +1,29 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int arr[1000] = {}; // 입력받은 수열을 저장
-	int dp[1000] = {}; // 해당 index가 부분수열의 마지막 원소라고 가정하였을 때 해당 부분 순열의 길이를 저장
-	int n, i, j, max, ans;
-	cin >> n;
-	for (i = 0; i < n; i++)
-		cin >> arr[i];
-	for (i = 0; i < n; i++) {
-		max = 0;
-		for (j = 0; j < i; j++) {
-			if (arr[j] < arr[i] && max < dp[j]) // 증가 여부 && 가장 긴 부분순열 여부
-				max = dp[j];
-		}
-		dp[i] = max + 1;
+	int N, A[1'005]{}, DP[1'005]{}; cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> A[i];
 	}
-	ans = dp[0];
-	for (i = 1; i < n; i++)
-		if (ans < dp[i])
-			ans = dp[i];
+
+	DP[0] = 1;
+	for (int i = 1; i < N; i++) {
+		int cur = 1;
+		for (int j = 0; j < i; j++) {
+			if (A[j] < A[i]) {
+				cur = max(cur, DP[j] + 1);
+			}
+		}
+		DP[i] = cur;
+	}
+
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+		ans = max(ans, DP[i]);
+	}
+
 	cout << ans;
 }
